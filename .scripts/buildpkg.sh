a0f7ac2476076079a99c3f010e1d8da4c1e433b1
Oliver Freyermuth <o.freyermuth@googlemail.com>
2021-01-12 22:16:20 +0100

    asd

--- .scripts/buildpkg.sh
+++ .scripts/buildpkg.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
-TOP_DIR=$(pwd)
-WORKING_DIR=${TOP_DIR}/debian/output
+export TOP_DIR=${GITHUB_WORKSPACE}
+export CCACHE_DIR=${TOP_DIR}/.ccache
+export WORKING_DIR=${TOP_DIR}/debian/output
 
 # Enter source package dir
 cd ${TOP_DIR}/source_dir
@@ -27,7 +28,7 @@ ccache -z
 
  # Create build user and fix permissions
 useradd buildci
-chown -R buildci. .
+chown -R buildci. ${WORKING_DIR} ${CCACHE_DIR}
 
 # Define buildlog filename
 BUILD_LOGFILE_SOURCE=$(dpkg-parsechangelog -S Source)
@@ -39,10 +40,12 @@ BUILD_LOGFILE="${WORKING_DIR}/${BUILD_LOGFILE_SOURCE}_${BUILD_LOGFILE_VERSION}_$
 # Build package as user buildci
 su buildci -c "eatmydata dpkg-buildpackage ${DB_BUILD_PARAM}" |& OUTPUT_FILENAME=${BUILD_LOGFILE} filter-output
 
-
 # Restore PWD to ${WORKING_DIR}
 cd ${WORKING_DIR}
 rm -rf ${WORKING_DIR}/${SOURCE_DIR}
+
+# Revert ownership for CCACHE_DIR
+chown -R $(id -nu). ${CCACHE_DIR}
 
 # Print ccache stats on job log
 ccache -s
