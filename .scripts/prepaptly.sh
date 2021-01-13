8699488c72efec34de32c3cdc35be5b8fb9eb146
Oliver Freyermuth <o.freyermuth@googlemail.com>
2021-01-13 02:42:32 +0100

    bla

--- .scripts/prepaptly.sh
+++ .scripts/prepaptly.sh
@@ -9,4 +9,13 @@ aptly repo create -distribution ${RELEASE} -component main ${PROJECT_NAME}
 aptly repo add ${PROJECT_NAME} ${PKGDIR}
 aptly repo show -with-packages ${PROJECT_NAME}
 
+ARCHITECTURES=$(aptly repo show -with-packages ${PROJECT_NAME} | \
+		awk 'BEGIN {FS="_"} /^Packages:/ {x=NR} (x && NR>x) {print $3}' | \
+		sort -u | tr '\n' ','); \
+
+aptly publish repo \
+		-skip-signing \
+		${ARCHITECTURES:+ -architectures=${ARCHITECTURES}} \
+		${PROJECT_NAME}
+
 find .aptly
