56cfc016b479d95ad353b86709eedbc1ec0a1c65
Oliver Freyermuth <o.freyermuth@googlemail.com>
2021-01-13 02:32:56 +0100

    bla

--- .scripts/prepaptly.sh
+++ .scripts/prepaptly.sh
@@ -8,3 +8,8 @@ mv *-debpkg/*.deb ${PKGDIR}
 aptly repo create -distribution ${RELEASE} -component main ${PROJECT_NAME}
 aptly repo add ${PROJECT_NAME} ${PKGDIR}
 aptly repo show -with-packages ${PROJECT_NAME}
+
+find
+ls -la
+
+cat /etc/aptly/index.html.template
