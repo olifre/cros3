db99ef48c6d1ed1338a1fc1443ce1547b692318e
Oliver Freyermuth <o.freyermuth@googlemail.com>
2021-01-15 00:59:53 +0100

    bla

--- .scripts/buildrpm.sh
+++ .scripts/buildrpm.sh
@@ -2,7 +2,7 @@
 
 mkdir build
 cp -ra source_dir/src/* build/
-cp *.spec build/
+cp source_dir/*.spec build/
 cd build
 
 yum install -y gcc rpm-build rpm-devel rpmlint make python bash coreutils diffutils patch rpmdevtools
