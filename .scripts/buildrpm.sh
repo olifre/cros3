1635b8d11b70a7a94894b51fd8ee604f449fc2a0
Oliver Freyermuth <o.freyermuth@googlemail.com>
2021-01-15 01:22:37 +0100

    bla

--- .scripts/buildrpm.sh
+++ .scripts/buildrpm.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -x
 
+yum install -y gcc rpm-build rpm-devel rpmlint make bash coreutils diffutils patch rpmdevtools
 rpmdev-setuptree
 
 tar cjf cros3-0.1.tar.bz2 -C source_dir/src .
 cp *.tar.bz2 ~/rpmbuild/SOURCES/
 cp source_dir/*.spec .
 
-yum install -y gcc rpm-build rpm-devel rpmlint make python bash coreutils diffutils patch rpmdevtools
 yum-builddep -y cros3.spec
 
 rpmbuild -ba cros3.spec
