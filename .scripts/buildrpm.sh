43968c79e86841bb4ef08a42f2c83ffe7485438b
Oliver Freyermuth <o.freyermuth@googlemail.com>
2021-01-15 01:11:05 +0100

    bla

--- .scripts/buildrpm.sh
+++ .scripts/buildrpm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 
 tar czf cros3-0.1.tar.bz2 -C source_dir/src .
+cp source_dir/*.spec .
 
 yum install -y gcc rpm-build rpm-devel rpmlint make python bash coreutils diffutils patch rpmdevtools
 yum-builddep -y cros3.spec
